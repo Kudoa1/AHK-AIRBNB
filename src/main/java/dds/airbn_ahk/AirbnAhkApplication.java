f54a36582dfee4798733d6b296985577b6977f46
Kudoa1 <Kudoagameryt@gmail.com>
2025-04-04 09:23:42 -0300

    Proyecto Inicial
    Java17-Springboot-Maven

---
+++ src/main/java/dds/airbn_ahk/AirbnAhkApplication.java
@@ -0,0 +1,13 @@
+package dds.airbn_ahk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnAhkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnAhkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dds/airbn_ahk/AirbnAhkApplicationTests.java
@@ -0,0 +1,13 @@
+package dds.airbn_ahk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnAhkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
